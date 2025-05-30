350ed39115e1532370f4266582742365ce143e1b
Robert Yaw Agyekum Addo <77078438+ROBERT-ADDO-ASANTE-DARKO@users.noreply.github.com>
2025-05-30 09:34:37 +0000

    Create __init__.py

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,5 @@
+from .transcription import TranscriptionService
+from .ocr import OCRService
+#from .translation import TranslationService
+
+__all__ = ["TranscriptionService", "OCRService"]#, "TranslationService"]
